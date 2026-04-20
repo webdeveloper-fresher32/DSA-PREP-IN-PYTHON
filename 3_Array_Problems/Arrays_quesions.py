31eff5cefd04061e43e1c05be658f8be8c19fd59
Ganesh Pirikirala <pirikiralaganesh1234@gmail.com>
2026-04-20 12:44:58 +0530

    Add DSA Pattern Recognition Table and implement array manipulation functions

--- 3_Array_Problems/Arrays_quesions.py
+++ 3_Array_Problems/Arrays_quesions.py
@@ -12,7 +12,63 @@
 
 
 def reverseArray(arr):
-    return arr[::-1]
+    left=0
+    right=len(arr)-1
+    while left<right:
+        arr[left],arr[right]=arr[right],arr[left]
+        left+=1
+        right-=1
+    return arr
+
+# print(reverseArray([1, 4, 3, 2, 6, 5]))
 
 
-print(reverseArray([1, 4, 3, 2, 6, 5]))
+# ------------------------------------------------------------------------------------------------------------------------------
+
+# Given an array of integers arr[], the task is to find the maximum and minimum elements in the array using the minimum number of comparisons.
+
+# Examples:
+
+# Input: arr[] = [3, 5, 4, 1, 9]
+# Output: [1, 9]
+# Explanation: The minimum element is 1, and the maximum element is 9.
+
+# Input: arr[] = [22, 14, 8, 17, 35, 3]
+# Output: [3, 35] 
+# Explanation: The minimum element is 3, and the maximum element is 35.
+
+
+# Brute force 
+
+def minAndMax(arr):
+    n=len(arr)
+    mini=arr[0]
+    maxi=arr[0]
+    for i in range(n):
+        if arr[i]<mini:
+            mini=arr[i]
+        if arr[i]>maxi:
+            maxi=arr[i]
+    return [mini,maxi]
+
+
+# print(minAndMax([3, 5, 4, 1, 9]))
+
+# -------------------------------------------------------------------------------------------------------------
+
+# Given an integer array arr[] and an integer k, your task is to find and return the kth smallest element in the given array.
+
+# Note: The kth smallest element is determined based on the sorted order of the array.
+
+# Input: arr[] = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10], k = 4
+# Output: 5
+# Explanation: 4th smallest element in the given array is 5.
+# Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
+# Output: 7
+# Explanation: 3rd smallest element in the given array is 7.
+
+def kthMinAndMax(arr,k):
+    #  on hold need to learn sorting techniques actually 
+    pass
+# print(kthMinAndMax([10, 5, 4, 3, 48, 6, 2, 33, 53, 10],4))
+
